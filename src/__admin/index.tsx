89bbb25b7bb1b9130bf5262c7fbcecaf4f4f1622
Puypuyy <115604612+Puypuyy@users.noreply.github.com>
2025-12-24 23:41:12 +0800

    1
    2

---
+++ src/__admin/index.tsx
@@ -0,0 +1,87 @@
+// Libraries
+import { Suspense } from 'react';
+import { Box, createTheme, ThemeProvider } from '@mui/system';
+import { CssBaseline } from '@mui/material';
+// Assets
+
+// Layouts
+
+
+export const Index = () => {
+	const pageTheme = createTheme({
+		palette: {
+			background: {
+				default: '#F0F5F9'
+			},
+			primary: {
+				main: '#256EFF',
+				dark: '#256EFF',
+				light: '#e9ebee',
+				lighter: '#f8f9fa',
+			},
+			secondary: {
+				main: '#256EFF',
+				dark: '#7aa3a1',
+				light: '#ffffff',
+			},
+			info: {
+				main: '#7aa3a1',
+				dark: '#506e6c',
+				light: '#b8d4d2',
+			},
+			error: {
+				main: '#f44336',
+				light: '#f6685e',
+				dark: '#d32f2f'
+			},
+			warning: {
+				main: '#ff9800',
+				dark: '#b26a00',
+				light: '#ffac33'
+			},
+			success: {
+				main: '#4caf50',
+				dark: '#388e3c',
+				light: '#81c784'
+			},
+			btnYellow:{
+				light: '#ffcd87',
+				main: '#ffb347',
+				color: '#f2f5fa'
+			},
+			text:{
+				main: '#256EFF',
+			},
+
+		},
+		typography: {
+			button: {
+				textTransform: 'none'
+			},
+			fontFamily: [
+			  '-apple-system',
+			  '"Inter"',
+			].join(','),
+		},
+	});	
+	// if (loc === '/pub') {
+	// 	// Core = SSORqx
+	// } else {
+    //     console.log('test')
+	// }
+
+	return (
+		<ThemeProvider theme= { pageTheme }>
+			<CssBaseline />
+            <Suspense fallback={
+                <Box height="100%" width="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+                    <Box mt={4}>
+                        <Box>loader....</Box>
+                    </Box>
+                </Box>
+            }>
+                <Box>Test Admin</Box>
+            </Suspense>
+		</ThemeProvider>
+	)
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Index as Admin } from './__admin/index.tsx'
+import { Index as Client } from './__client/index.tsx'
+import { Index as Marketing } from './__marketing/index.tsx'
+import { Index as Public } from './__public/index.tsx'
+import './App.css'
+
+const loc = window.location.pathname
+const subdir = loc.split('/')[2]
+let Core
+
+console.log(subdir)
+
+if (subdir === 'developerAccess') {
+	Core = Admin
+} else if (subdir === 'wedorg') {
+	Core = Client
+} else if (subdir === 'invitation') {
+	Core = Public
+} else {
+	Core = Marketing
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<Core />
+	</StrictMode>,
 )
